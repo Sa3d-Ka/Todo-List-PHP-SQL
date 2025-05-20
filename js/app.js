a48b034324076ec5190daaf5aa41a4d918a064aa
Saad Kanani <saad.kanani.off@gmail.com>
2025-05-20 23:24:04 +0100

    addTask

--- js/app.js
+++ js/app.js
@@ -36,3 +36,35 @@ function afficherTaches(tasks) {
     listContainer.append(li);
   });
 }
+
+$("#addTaskBtn").click(() => {
+  const title = $("#input-box").val();
+  console.log(title);
+
+  $.ajax({
+    url: "server/api/add_task.php",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify({ title }),
+    dataType: "json",
+    success: (t) => ajouterTacheDOM(t),
+    error: (jq) => console.error("Add error:", jq.status),
+  });
+  $("#input-box").val('');
+});
+
+function ajouterTacheDOM(t) {
+  const li = $(`
+            <li data-id="${t.id}">
+                <div class="taskC">
+                    <i class="fa-regular fa-circle-check toggle"></i>
+                    <span>${t.title}</span>
+                </div>
+                <div class="actions">
+                    <button class="deleteBtn"><i class="fa-regular fa-trash-can"></i></button>
+                </div>
+            </li>
+        `);
+
+  listContainer.append(li);
+}
